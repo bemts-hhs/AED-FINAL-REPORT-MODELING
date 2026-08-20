814dd103bbc1ef4111835511cda394b4951a0785
bemts-hhs <nicolas.foss@hhs.iowa.gov>
2026-08-20 09:50:53 -0500

    removed comments from installed packages to ensure renv doesn't remove pak

--- R/setup.R
+++ R/setup.R
@@ -3,23 +3,23 @@
 ###___________________________________________________________________________
 
 ## install pak ----
-# install.packages("pak")
+install.packages("pak")
 
 ## install tidyverse, tidymodels, renv is loaded via Positron project template ----
-# pak::pak("tidyverse")
-# pak::pak("tidymodels")
-# pak::pak("cpp11") # required for readxl?
-# pak::pak("progress") # required for readxl?
-# pak::pak("finetune")
-# pak::pak("xgboost")
-# pak::pak("ranger")
-# pak::pak("lme4")
-# pak::pak("glmnet")
-# pak::pak("RcppArmadillo")
-# pak::pak("RcppEigen")
-# pak::pak("themis")
-# pak::pak("lorax")
-# pak::pak("sessioninfo")
+pak::pak("tidyverse")
+pak::pak("tidymodels")
+pak::pak("cpp11") # required for readxl?
+pak::pak("progress") # required for readxl?
+pak::pak("finetune")
+pak::pak("xgboost")
+pak::pak("ranger")
+pak::pak("lme4")
+pak::pak("glmnet")
+pak::pak("RcppArmadillo")
+pak::pak("RcppEigen")
+pak::pak("themis")
+pak::pak("lorax")
+pak::pak("sessioninfo")
 
 ## access the environment variable ----
 aed_file <- Sys.getenv("aed_path")
